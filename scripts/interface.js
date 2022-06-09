9839ca77cc0992673155f8c1b99737e61d03e425
rafa358 <rafael.nogueira.santana98@gmail.com>
2022-06-09 12:03:52 -0300

    game finished

---
+++ scripts/interface.js
@@ -0,0 +1,54 @@
+
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    let squares = document.querySelectorAll('.square');
+
+    squares.forEach((square)=>{
+        square.addEventListener('click', handleClick);
+    })
+
+
+})
+
+
+function handleClick(event) {
+
+    let square = event.target;
+    let position = square.id;
+
+    if (handleMove(position)){
+        updateSquare(position);
+
+
+        setTimeout (()=> {
+            alert (' O JOGO ACABOU! O VENCEDOR FOI ' + playerTime)
+        }, 15);    
+    }
+
+    updateSquare(position);
+}
+
+function updateSquare(position){
+    let square = document.getElementById(position.toString());
+    let symbol = board[position];
+    square.innerHTML = `<div class='${symbol}'></div>`
+}
+
+
+// ou assim,
+
+// function updateSquares() {
+
+//     let squares = document.querySelectorAll('.square');
+
+//     squares.forEach((square)=>{
+//        let position = square.id;
+//        let symbol = board[position]
+
+//        if (symbol != ''){
+//            square.innerHTML = `<div class='${symbol}'></div>`
+//        }
+//     })
+
+
+//}
